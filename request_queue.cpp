07f506f9f834a30b8b37bde2f474a62213737c43
Emil <e.zainullin2@yandex.ru>
2020-10-30 01:08:09 +0500

    Revert "Revert "split the project into files""
    This reverts commit 917868481e8eb5c50ed5e5e52bd5fe0788fc806b.

---
+++ request_queue.cpp
@@ -0,0 +1,42 @@
+#include "request_queue.h"
+
+using namespace std;
+
+RequestQueue::RequestQueue(const SearchServer& search_server)
+    : num_of_null_query_(0)
+    , search_server_(search_server){
+
+}
+
+vector<Document> RequestQueue::AddFindRequest(const string& raw_query, DocumentStatus status) {
+    vector<Document> request = search_server_.FindTopDocuments(raw_query, status);
+    QueryResult query_res = { request };
+    AddRequest(query_res);
+
+    return request;
+}
+
+vector<Document> RequestQueue::AddFindRequest(const string& raw_query) {
+    vector<Document> request = search_server_.FindTopDocuments(raw_query);
+    QueryResult query_res = { request };
+    AddRequest(query_res);
+
+    return request;
+}
+
+int RequestQueue::GetNoResultRequests() const {
+    return num_of_null_query_;
+}
+
+void RequestQueue::AddRequest(const QueryResult& request) {
+    if (static_cast<int>(requests_.size()) == sec_in_day_) {
+        if (requests_.front().result.empty()) {
+            --num_of_null_query_;
+        }
+        requests_.pop_front();
+    }
+    requests_.push_back(request);
+    if (request.result.empty()) {
+        ++num_of_null_query_;
+    }
+}
